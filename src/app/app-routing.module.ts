43c38c6fa95ed8eeace20acda78ac5abd5239406
gprocida <gprocida@xeniaprogetti.it>
2022-07-01 11:25:01 +0200

    integrate bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { GallaghersComponent } from './gallaghers/gallaghers.component';
+
+const routes: Routes = [
+  { path: 'gallaghers', component: GallaghersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
